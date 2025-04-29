f86588fb51925a4ed2abc20601031945f7ccf12c
Viraj Merai <merai.viraj@gmail.com>
2025-04-29 16:53:25 -0400

    add /api in last of backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api", // change this in production
-  baseURL:import.meta.env.VITE_BACKEND_URL,
+  baseURL:`${import.meta.env.VITE_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
