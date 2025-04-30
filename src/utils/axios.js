363abe4336370c6e9d3e08a9bc6cbdf8a9dcc64e
Viraj Merai <merai.viraj@gmail.com>
2025-04-30 16:21:45 -0400

    minor change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:5000/api", // change this in production
+  // baseURL: "http://localhost:5000/api", 
   baseURL:`${import.meta.env.VITE_BACKEND_URL}api`,
   headers: {
     "Content-Type": "application/json",
