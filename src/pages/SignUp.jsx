7c7020a719dddddb8d6fedd25948349630a5a1b6
Viraj Merai <merai.viraj@gmail.com>
2025-05-01 13:36:43 -0400

    button changes'

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -94,26 +94,7 @@ function SignUp() {
             } text-white`}
           >
             {loading ? (
-              <svg
-                className="animate-spin h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                ></path>
-              </svg>
+              "Loading..."
             ) : (
               "Sign Up"
             )}
